059012d9d3916b4f9502ab0aaa2395ef3ae651ad
Aguirres.K <55071994+kassiogto@users.noreply.github.com>
2020-09-21 00:27:22 -0300

    Update authe.tsx

--- src/service/authe.tsx
+++ src/service/authe.tsx
@@ -2,6 +2,7 @@ import API from './api';
 
 
 export async function signIn(data:object) {
+    //  Coloque Sua Rota de Login no lugar de "/login"
     const response = await API.post("/login", data)
 
     return response;
